47a4555d06521a60b69ef8b7d9e6e78236f20ad4
Alejandro Vela <avelaproyt@gmail.com>
2024-03-29 01:34:39 -0600

    😎 feat: refactor UI/UX, added completed states and canDelete states to the task component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-
-import './App.css'
-import Task from "./utils/task.jsx"
+import "./App.css";
+import Task from "./utils/task.jsx";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-5xl py-10'>Remind-me 📝</h1>
-      <header className='flex flex-row rounded-2xl outline outline-neutral-600 outline-2 bg-neutral-800 px-2 py-1 w-max mx-auto'>
-        <p className='text-xl px-3'>Filter:</p>
-        <button id='filterButton' className='bg-red-500 rounded-2xl px-3 glass bg-opacity-40'>Not completed</button>
-      </header>
+      <h1 className="text-5xl py-10">Remind-me 📝</h1>
+      <input
+        placeholder="Insert title here."
+        className="p-2 rounded-2xl text-center outline-neutral-700 bg-neutral-800 outline-2 outline focus:outline-blue-600 focus:outline-3 w-96"
+      ></input>
 
-      <div className="py-12 grid md:grid-cols-2">
-        {Task(1, "Tarea exposicion y comprension", "Terminar la tarea que es de la pagina 92 a la 102")}
-        {Task(1, "Tarea exposicion y comprension", "Terminar la tarea que es de la pagina 92 a la 102")}
-        {Task(1, "Tarea exposicion y comprension", "Terminar la tarea que es de la pagina 92 a la 102")}
+      <input
+        placeholder="Insert description here."
+        className="p-2 mt-2 md:mt-0 lg:mt-0 md:ml-6 text-center rounded-2xl outline-neutral-700 bg-neutral-800 outline-2 outline focus:outline-blue-600 focus:outline-3 w-96"
+      ></input>
+
+      <div className="py-12 grid md:grid-cols-2 justify-center">
+        {Task(
+          1,
+          "Tarea exposicion y comprension",
+          "Terminar la tarea que es de la pagina 92 a la 102"
+        )}
+
+        {Task(
+          1,
+          "Tarea exposicion y comprension",
+          "Terminar la tarea que es de la pagina 92 a la 102"
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
